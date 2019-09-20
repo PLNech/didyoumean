48225244ce5fe5e69ef9bccae48d060b4c0cdc84
Paul-Louis NECH <pln@algolia.com>
2019-09-20 11:22:27 +0200

    feat: Integration tests

---
+++ didyoumean3/test.py
@@ -0,0 +1,23 @@
+import unittest
+
+from didyoumean3.didyoumean import did_you_mean
+
+mock_meal = ("Aubergines sautées", "sautéed eggplants", 10)
+
+
+class IntegrationTestCase(unittest.TestCase):
+    """ Integration Tests for didyoumean. """
+    corrections = [
+        ("Aubergine sautee", "Aubergine sautée"),
+        ("Pommes de terre sautees", "Pommes de terre sautées"),
+        ("steak hache", "steak haché"),
+        ("télégrafe", "télégraphe"),
+    ]
+
+    def setUp(self):
+        pass
+
+    def test_simple(self):
+        for (query, corrected) in self.corrections:
+            self.assertEqual(corrected, did_you_mean(query, "fr"),
+                             "%s should correct to %s" % (query, corrected))
